af96ddedaeda0ba5075fd8f8969da1aee26ea477
Varzil <varzilthakkarvvv@gmail.com>
2024-02-14 22:17:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+const data = require('./data.json');
 
-function App() {
+function lmao(){
+  return data[Math.floor(Math.random() * data.length)];
+}
+
+const Flashcard = ({ word, meaning }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [height, setHeight] = useState('initial')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    className={`container card ${isFlipped ? 'flip' : ''}`}
+    style={{ height: height }}
+    onClick={() => setIsFlipped(!isFlipped)}
+  >
+      <div className="front">
+        <h3>{word}</h3>
+      </div>
+      <div className="back">
+        <h3>{meaning}</h3>
+      </div>
     </div>
   );
-}
+};
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className='topic'>
+      <h1>Flashcards</h1>
+      </div>
+        <Flashcard key={lmao()} word={lmao().Word} meaning={lmao().Meaning} />
+    </div>
+  );
+};
 
 export default App;
